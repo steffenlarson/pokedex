fd919a864272e532e1300ee03bfd60e4eaafbb1a
Steffen Larson <steffman13@gmail.com>
2021-01-06 17:05:36 -0700

    Adding Notes

--- app/Controllers/PokeapiController.js
+++ app/Controllers/PokeapiController.js
@@ -1,24 +1,32 @@
+// NOTE this imports the proxystate from appstate to be used. A copy of data to be used.
 import { ProxyState } from "../AppState.js"
+// NOTE This imports the PokeService from the services file.
 import { pokeapiService } from "../Services/PokeapiServices.js"
 
-
+// NOTE this function draws the pokemon onto the html.
 function _drawPokemon() {
   // console.log('drawing')
+  // NOTE This template creates an empty template
   let template = ''
+  // NOTE this for each of the pokemon places them into a template. from the appstate. the proxy.
   ProxyState.pokeapiPokemon.forEach(p => {
     template += `<li class="action" onclick="app.pokeapiPokemon.getPokemon('${p.name}')">${p.name}</li>`
   })
+  // NOTE this grabs and injects the above template into the place where in the html "api-pokemon" is (id) identified.
   document.getElementById("api-pokemon").innerHTML = template
 }
 
+// NOTE this creates a clas for the controller and exports it for use elsewhere
 export default class PokeapiController {
+  // NOTE the constructor builds things on page load.
   constructor() {
-
+    // NOTE this line activates a listener and on the change of pokeapiPokemon activates _drawPokemon
     ProxyState.on('pokeapiPokemon', _drawPokemon)
 
-
+    // NOTE this calls the function getAllPokemon. which tries to grab all the data from the first api
     this.getAllPokemon()
   }
+  // NOTE this is the getAllPokemon function. In the controller it only calls over to the service to run it. It is set up with a try catch. and if The call to the service fails, it returns an error.
   getAllPokemon() {
     try {
       pokeapiService.getAllPokemon()
--- app/Services/PokeapiServices.js
+++ app/Services/PokeapiServices.js
@@ -1,11 +1,17 @@
+// NOTE these all import different things. the Proxystate in the appstate, the Pokemon Model from the models, and the pokeApi from the axios service.
 import { ProxyState } from "../AppState.js";
 import Pokemon from "../Models/Pokemon.js";
 import { pokeApi } from "../Services/AxiosServices.js";
 
+// NOTE The service is where all of the business logic runs. All of the actual functions (besides draw) are stored here. 
+// NOTE The class is built so that it can be sent off repeatedly whenever a function is needed. 
 class PokeapiService {
 
+  // NOTE this is an asynchronous function. These run until they get stuck and then the function waits for a response from a server. Then it completes. 
   async getAllPokemon() {
+    // NOTE Res means results then you await for the request sent to the api returns a result.
     let res = await pokeApi.get('')
+    // NOTE 
     ProxyState.pokeapiPokemon = res.data.results
   }
 }
